0ea4116f97b7ebeabf6d4486e2a7a52bcaba4d26
sebsfdez <157440937+sebsfdez@users.noreply.github.com>
2025-03-08 17:03:58 -0600

    Add files via upload

---
+++ empresa/src/Cubiculos.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author sebas
+ */
+public class Cubiculos {
+    private String privados;
+    private String publicos;
+
+    public Cubiculos(String privados, String publicos) {
+        this.privados = privados;
+        this.publicos = publicos;
+    }
+    
+    
+    
+}
---
+++ empresa/src/Empleados.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author sebas
+ */
+public class Empleados {
+    private String nombre;
+    private int ID;
+
+public String empleados(){
+return;
+}
+    public Empleados(String nombre, int ID) {
+        this.nombre = nombre;
+        this.ID = ID;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+    
+    
+}
+
