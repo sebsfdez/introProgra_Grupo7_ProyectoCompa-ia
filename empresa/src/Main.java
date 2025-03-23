ea8459bc30e0165a56ee6dae6bc54fbde6970d95
sebsfdez <157440937+sebsfdez@users.noreply.github.com>
2025-03-23 16:04:06 -0600

    Update Main.java

--- empresa/src/Main.java
+++ empresa/src/Main.java
@@ -14,12 +14,58 @@ public class Main {
      */
     public static void main(String[] args) {
         //conexion con constructores
-        Empleados empleados = new Empleados("",0);
-        Cubiculos cubiculos = new Cubiculos("","");
-        Parqueo parqueo = new Parqueo("","","","");
-        ReservEspaciosR reservEspaciosR = new ReservEspaciosR("","","","","","","");
-        SalaDeReunion salaDeReunion = new SalaDeReunion("","",0);
+        Empleados empleados = new Empleados("", 0);
+        Cubiculos cubiculos = new Cubiculos("", "");
+        Parqueo parqueo = new Parqueo("", "", "", "");
+        ReservEspaciosR reservEspaciosR = new ReservEspaciosR("", "", "", "", "", "", "");
+        SalaDeReunion salaDeReunion = new SalaDeReunion("", "", 0);
 
+        
+        JOptionPane.showMessageDialog(null, "¡Bienvenido!");
+        while (true) {
+            //menu principal que muestre al recepcionista de la empresa
+
+            int opciones = Integer.parseInt(JOptionPane.showInputDialog("Opciones:\n"
+                    + " 1. Empleados\n "
+                    + "2. Cubiculos\n "
+                    + "3. Parqueo\n "
+                    + "4. Reserva de espacios recreativos\n "
+                    + "5. Sala de reuniones\n "
+                    + "6. Salir"));
+
+            switch (opciones) {
+                case 1:
+                    JOptionPane.showMessageDialog(null, "Elegiste la opcion empleados:");
+                    //Agregar llamados de modulos de empleados, etc
+                    break;
+                case 2:
+                    JOptionPane.showMessageDialog(null, "Elegiste la opcion Cubiculos:");
+                    //Agregar llamados de modulos de cubiculos, etc
+                    break;
+                case 3:
+                    JOptionPane.showMessageDialog(null, "Elegiste la opcion Parqueo:");
+                    //Agregar llamados de modulos de parqueo, etc
+                    break;
+                case 4:
+                    JOptionPane.showMessageDialog(null, "Elegiste la opcion Reserva de espacios recreativos:");
+                    //Agregar llamados de modulos ReserEspaciosR, etc
+                    break;
+                case 5:
+                    JOptionPane.showMessageDialog(null, "Elegiste la opcion Sala de reuniones:");
+                    //Agregar llamados de modulos SalaDeReunion, etc
+                    break;
+
+            }
+            if (opciones > 6 || opciones < 1) {
+                JOptionPane.showMessageDialog(null, "¡Opcion invalida! Verifique que se encuentre entre las opciones");
+
+            }
+
+            if (opciones == 6) {
+                JOptionPane.showMessageDialog(null, "Elegiste salir");
+                break;
+            }
+        }
     }
     
 }
