8f9ce70b5a453b0db8f19e903f68eafd1605d2b1
Priscilla Mayorga <priscilla55m10@gmail.com>
2025-04-08 14:31:05 -0600

    Update ReservEspaciosR.java
    Quite los atributos, get, y set que no estábamos usando

--- empresa/src/ReservEspaciosR.java
+++ empresa/src/ReservEspaciosR.java
@@ -14,13 +14,6 @@ import javax.swing.JPanel;
  */
 public class ReservEspaciosR {
 
-    private String charla;
-    private String capacitacion;
-    private String mesaPingP;
-    private String mesaBillar;
-    private String futbol;
-    private String basquet;
-    private String tenis;
     private String canchaFut1 [][] = new String[7][3];
     private String canchaFut2 [][] = new String[7][3];
     private String canchaTenis1 [][] = new String [2][3];
@@ -82,61 +75,6 @@ public class ReservEspaciosR {
         }
     }
 
-    public String getCharla() {
-        return charla;
-    }
-
-    public void setCharla(String charla) {
-        this.charla = charla;
-    }
-
-    public String getCapacitacion() {
-        return capacitacion;
-    }
-
-    public void setCapacitacion(String capacitacion) {
-        this.capacitacion = capacitacion;
-    }
-
-    public String getMesaPingP() {
-        return mesaPingP;
-    }
-
-    public void setMesaPingP(String mesaPingP) {
-        this.mesaPingP = mesaPingP;
-    }
-
-    public String getMesaBillar() {
-        return mesaBillar;
-    }
-
-    public void setMesaBillar(String mesaBillar) {
-        this.mesaBillar = mesaBillar;
-    }
-
-    public String getFutbol() {
-        return futbol;
-    }
-
-    public void setFutbol(String futbol) {
-        this.futbol = futbol;
-    }
-
-    public String getBasquet() {
-        return basquet;
-    }
-
-    public void setBasquet(String basquet) {
-        this.basquet = basquet;
-    }
-
-    public String getTenis() {
-        return tenis;
-    }
-
-    public void setTenis(String tenis) {
-        this.tenis = tenis;
-    }
 
     //opcion 4 reserva de espacios recreativos
     public void mostrarOpcionesRER() { //RER: reserva de espacios recreativos
