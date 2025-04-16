2507227c3b315e8c28c78081a426f816dbbbeeaa
sebsfdez <157440937+sebsfdez@users.noreply.github.com>
2025-04-15 21:27:25 -0600

    Add files via upload

---
+++ empresa/src/Cubiculos.java
@@ -0,0 +1,271 @@
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import javax.swing.JOptionPane;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author sebas
+ */
+public class Cubiculos {
+
+    private String privados;
+    private String publicos;
+    private String cubiculoPriv [][] = new String [4][3];
+    private String cubiculoPubli [][] = new String [4][3];
+
+    public Cubiculos(String privados, String publicos) {
+        this.privados = privados;
+        this.publicos = publicos;
+
+        //Rellenando espacios del cubiculo privado
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 3; j++) {
+                cubiculoPriv[i][j] = "[O]";
+            }
+        }
+     //Rellenando espacios del cubiculo publico
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 3; j++) {
+                cubiculoPubli[i][j] = "[O]";
+            }
+        }
+    }
+
+    public String getPrivados() {
+        return privados;
+    }
+
+    public void setPrivados(String privados) {
+        this.privados = privados;
+    }
+
+    public String getPublicos() {
+        return publicos;
+    }
+
+    public void setPublicos(String publicos) {
+        this.publicos = publicos;
+    }
+
+    //opcion 2 cubiculos, aca pongo igual un subMenu con las opciones de las acciones posibles a ejecutar
+    public void mostrarOpcionesCubiculos() {
+        while (true) {
+            int opcionesCubiculos = Integer.parseInt(JOptionPane.showInputDialog(null, "Elegiste la opcion *Cubiculos* | Que deseas hacer?\n"
+                    + " 1. Liberar espacio cubiculo privado\n"
+                    + " 3. Reservar cubiculos\n"
+                    + " 4. Regresar al menu principal"));//agregar más opciones si es necesario porque me falto poner más
+            switch (opcionesCubiculos) {
+                case 1:
+                    liberarEspacioCubiculo();
+                    break;//meter más cases si es necesario
+                case 3:
+                    ReservarCubiculo();
+                    break;//meter más cases si es necesario
+             
+
+            }
+            if (opcionesCubiculos > 4 || opcionesCubiculos < 1) {
+                JOptionPane.showMessageDialog(null, "¡Opcion invalida! Verifique que se encuentre entre las opciones");
+
+            }
+            if (opcionesCubiculos == 4) {
+                break;
+            }
+        }
+    }
+
+    //acciones para el recepcionista al ingresar a cada opcion del menu principal, 
+    //deberia de llamarlo en el main dentro del menu de opciones
+    public boolean liberarEspacioCubiculo() {
+        while (true) {
+            int cubiculoLiberar = Integer.parseInt(JOptionPane.showInputDialog("Cual cubiculo desea liberar:\n"
+                    + "1. Cubiculo privado\n"
+                    + "2. Cubiculo publico\n"
+                    + "3. Regresar"));
+            switch (cubiculoLiberar) {
+                case 1://cancha 1
+                    int filaCubPrivado = Integer.parseInt(JOptionPane.showInputDialog("Digite la fila en que se encuentra: "));
+                    int columnaCubPrivado = Integer.parseInt(JOptionPane.showInputDialog("Digite la columna que se encuentra: "));
+                    if (cubiculoPriv[filaCubPrivado][columnaCubPrivado] == "[P]") {
+                        cubiculoPriv[filaCubPrivado][columnaCubPrivado] = "[O]";
+                        JOptionPane.showMessageDialog(null, "¡Espacio liberado exitosamente!");
+
+                    } else if (cubiculoPriv[filaCubPrivado][columnaCubPrivado] == "[O]") {
+                        JOptionPane.showMessageDialog(null, "¡El espacio ya estaba liberado!");
+                    } else {
+                        JOptionPane.showMessageDialog(null, "No es permitido liberar este espacio");
+                    }
+                    break;
+                case 2://cancha 2
+                    int filaCubPublico = Integer.parseInt(JOptionPane.showInputDialog("Digite la fila en que se encuentra: "));
+                    int columnaCubPublico = Integer.parseInt(JOptionPane.showInputDialog("Digite la columna que se encuentra: "));
+                    if (cubiculoPubli[filaCubPublico][columnaCubPublico] == "[P]") {
+                        cubiculoPubli[filaCubPublico][columnaCubPublico] = "[O]";
+                        JOptionPane.showMessageDialog(null, "¡Espacio liberado exitosamente!");
+
+                    } else if (cubiculoPubli[filaCubPublico][columnaCubPublico] == "[O]") {
+                        JOptionPane.showMessageDialog(null, "¡El espacio ya estaba liberado!");
+                    } else {
+                        JOptionPane.showMessageDialog(null, "No es permitido liberar este espacio");
+                    }
+                    break;
+
+            }
+            if (cubiculoLiberar> 3 || cubiculoLiberar < 1) {
+                JOptionPane.showMessageDialog(null, "¡Opcion invalida! Verifique que se encuentre entre las opciones");
+
+            }
+            if (cubiculoLiberar== 3) {
+                break;
+            }
+        }
+        return true;
+    }
+    
+    
+
+    public boolean ReservarCubiculo() {
+        LocalTime horaInicio = LocalTime.of(9, 0);  // 9:00 AM
+        LocalTime horaFin = LocalTime.of(18, 0);  // 6:00 PM
+
+        //formato de 24 horas
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("HH:mm");
+        //variable donde almacenar hora
+        LocalTime hora = null;
+        //pedir datos
+        String datos = JOptionPane.showInputDialog("Que hora desea reservar(HH:mm): ");
+        //Los datos convertidos en localTime
+        hora = LocalTime.parse(datos, formato);
+
+        if (hora.isBefore(horaInicio) || hora.isAfter(horaFin)) {
+            JOptionPane.showMessageDialog(null, "No hay cupos a la hora que desea reservar");
+        } else {//prefijar columnas array 1
+            cubiculoPriv[0][0] = "     ";
+            cubiculoPriv[0][1] = "*1*";
+            cubiculoPriv[0][2] = "*2*";
+            //prefijar filas array 1 
+            cubiculoPriv[1][0] = "*1*";
+            cubiculoPriv[2][0] = "*2*";
+            cubiculoPriv[3][0] = "*3*";
+
+            //prefijar columnas array 1
+            cubiculoPubli[0][0] = "     ";
+            cubiculoPubli[0][1] = "*1*";
+            cubiculoPubli[0][2] = "*2*";
+            //prefijar filas array 1 
+            cubiculoPubli[1][0] = "*1*";
+            cubiculoPubli[2][0] = "*2*";
+            cubiculoPubli[3][0] = "*3*";
+
+            while (true) {
+                int opcionesCubiculo = Integer.parseInt(JOptionPane.showInputDialog("Opciones: \n"
+                        + "1. Reservar cubiculo \n"
+                        + "2. Visualizar espacios\n"
+                        + "3. Regresar"));
+
+                switch (opcionesCubiculo) {
+                    case 1://Reserva
+                        while (true) {
+                            int cubiculoReserva = Integer.parseInt(JOptionPane.showInputDialog("Cual cubiculo desea reservar :\n"
+                                    + "1. Cubiculo privado\n"
+                                    + "2. Cubiculo publico\n"
+                                    + "3. Regresar"));
+                            switch (cubiculoReserva) {
+                                case 1://cancha 1
+                                    int filaCubPrivado = Integer.parseInt(JOptionPane.showInputDialog("Digite la fila en que se encuentra: "));
+                                    int columnaCubPrivado = Integer.parseInt(JOptionPane.showInputDialog("Digite la columna que se encuentra: "));
+                                    if (cubiculoPriv[filaCubPrivado][columnaCubPrivado] == "[O]") {
+                                        cubiculoPriv[filaCubPrivado][columnaCubPrivado] = "[P]";
+                                        JOptionPane.showMessageDialog(null, "¡Espacio asignado exitosamente!");
+
+                                    } else {
+                                        JOptionPane.showMessageDialog(null, "¡Espacio ocupado!");
+                                    }
+                                    break;
+                                case 2://cancha 2
+                                    int filaCubPublico = Integer.parseInt(JOptionPane.showInputDialog("Digite la fila en que se encuentra: "));
+                                    int columnaCubPublico = Integer.parseInt(JOptionPane.showInputDialog("Digite la columna que se encuentra: "));
+                                    if (cubiculoPubli[filaCubPublico][columnaCubPublico] == "[O]") {
+                                        cubiculoPubli[filaCubPublico][columnaCubPublico] = "[P]";
+                                        JOptionPane.showMessageDialog(null, "¡Espacio asignado exitosamente!");
+
+                                    } else {
+                                        JOptionPane.showMessageDialog(null, "¡Espacio ocupado!");
+                                    }
+                                    break;
+
+                            }
+                            if (cubiculoReserva > 3 || cubiculoReserva < 1) {
+                                JOptionPane.showMessageDialog(null, "¡Opcion invalida! Verifique que se encuentre entre las opciones");
+
+                            }
+                            if (cubiculoReserva == 3) {
+                                break;
+                            }
+                        }
+
+                        break;
+                    case 2://visualizar
+                        while (true) {
+                            int cubiculoVisualizar = Integer.parseInt(JOptionPane.showInputDialog("Cual cubiculo desea visualizar:\n"
+                                    + "1. Cubiculo privado\n"
+                                    + "2. Cubiculo publico\n"
+                                    + "3. Regresar"));
+                            switch (cubiculoVisualizar) {
+                                case 1:
+                                    String mostrarEspacios1 = "";
+                                    for (int i = 0; i < 4; i++) {
+                                        for (int j = 0; j < 3; j++) {
+                                            mostrarEspacios1 += cubiculoPriv[i][j] + "   ";
+                                        }
+                                        mostrarEspacios1 += "\n";
+                                    }
+                                    JOptionPane.showMessageDialog(null, "Cubiculo privado:\n"
+                                            + mostrarEspacios1);
+                                    break;
+                                case 2:
+                                    String mostrarEspacios2 = "";
+                                    for (int i = 0; i < 4; i++) {
+                                        for (int j = 0; j < 3; j++) {
+                                            mostrarEspacios2 += cubiculoPubli[i][j] + "   ";
+                                        }
+                                        mostrarEspacios2 += "\n";
+                                    }
+                                    JOptionPane.showMessageDialog(null, "Cubiculo publico:\n"
+                                            + mostrarEspacios2);
+                                    break;
+                            }
+
+                            if (cubiculoVisualizar > 3 || cubiculoVisualizar < 1) {
+                                JOptionPane.showMessageDialog(null, "¡Opcion invalida! Verifique que se encuentre entre las opciones");
+
+                            }
+                            if (cubiculoVisualizar == 3) {
+                                break;
+                            }
+
+                        }
+
+                        break;
+
+                }
+
+                if (opcionesCubiculo > 3 || opcionesCubiculo < 1) {
+                    JOptionPane.showMessageDialog(null, "¡Opcion invalida! Verifique que se encuentre entre las opciones");
+
+                }
+                if (opcionesCubiculo == 3) {
+                    break;
+                }
+            }
+        }
+
+        return true;
+
+    }
+}
---
+++ empresa/src/Empleados.java
@@ -0,0 +1,247 @@
+
+import javax.swing.JOptionPane;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author sebas
+ */
+public class Empleados {
+    
+    private String nombre;
+    private int ID;
+    private String rol;
+    private int cantidadInicial = 10;//cantidad inicial
+    private String empleados[][] = new String[cantidadInicial][3];
+    private int contador = 0;//contador que sirve para verificar que hayan empleados registrados y el numero que se lleva actualmente
+
+    //private String 
+    public Empleados(String nombre, int ID, String almacenNombres, int almacenID, String rol) {
+        this.nombre = nombre;
+        this.ID = ID;
+        this.rol = rol;
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    //opcion 1 la cual es llamada en el main:
+    public void mostrarOpcionesEmpleados() {
+        while (true) {
+            //opciones mostradas
+            int opcionesEmpleados = Integer.parseInt(JOptionPane.showInputDialog(null, "Elegiste la opcion *empleados* | Que deseas hacer?\n"
+                    + "1. Agregar\n"
+                    + "2. Eliminar\n"
+                    + "3. Mostrar empleados\n"
+                    + "4. Buscar empleado\n"
+                    + "5. Modificar informacion de empleado\n"
+                    + "6. Consultar reservas de empleado\n"
+                    + "7. Regresar al menu principal"));
+
+            //Agregar llamados de modulos de empleados si hacen falta, etc
+            switch (opcionesEmpleados) {
+                case 1:
+                    agregar();
+                    //llamado de accion o metodo en donde debe ir funcionalidad
+                    break;
+                case 2:
+                    eliminar();
+                    //llamado de accion o metodo en donde debe ir funcionalidad
+                    break;
+                case 3:
+                    mostrarEmpleados();
+                    //llamado de accion o metodo en donde debe ir funcionalidad
+                    break;
+                case 4:
+                    buscarEmpleado();
+                    ///llamado de accion o metodo en donde debe ir funcionalidad
+                    break;
+                case 5:
+                    modificar();
+                    //llamado de accion o metodo en donde debe ir funcionalidad
+                    break;
+                case 6:
+                    consultarReserva();
+                    break;
+                
+
+            }
+            if (opcionesEmpleados > 7 || opcionesEmpleados < 1) {
+                JOptionPane.showMessageDialog(null, "¡Opcion invalida! Verifique que se encuentre entre las opciones");
+            }
+            if (opcionesEmpleados == 7) {
+                break;
+            }
+        }
+    }
+
+    //acciones para el recepcionista al ingresar a cada opcion del menu principal, 
+    //deberia de llamarlo en el main dentro del menu de opciones
+    public String[] agregar() {
+    while (true) {
+        if (contador >= empleados.length) {
+            int nuevoTamanio = empleados.length * 2;
+            String[][] arrayEmpleadoNuevo = new String[nuevoTamanio][3];
+            for (int i = 0; i < empleados.length; i++) {
+                arrayEmpleadoNuevo[i] = empleados[i];
+            }
+            empleados = arrayEmpleadoNuevo;
+        }
+
+        String nombre = JOptionPane.showInputDialog("Ingrese nombre:");
+        String id = JOptionPane.showInputDialog("Ingrese ID:");
+        String rol = JOptionPane.showInputDialog("Ingrese rol:");
+
+        if (nombre.isEmpty() || id.isEmpty() || rol.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Datos erróneos. Vuelva a intentarlo.");
+            return null;
+        }
+
+        empleados[contador][0] = nombre;
+        empleados[contador][1] = id;
+        empleados[contador][2] = rol;
+
+        contador++;
+
+        JOptionPane.showMessageDialog(null, "¡Agregado exitosamente! Total de empleados: " + contador);
+
+        String salida = JOptionPane.showInputDialog("¿Desea agregar otro empleado? (si/no)");
+        if (salida.equalsIgnoreCase("no")) {
+            return new String[]{nombre, id, rol};  // Retornamos los datos del último empleado
+        }
+    }
+}
+
+
+    public boolean eliminar() {
+        while (true) {
+            int borrar = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero del empleado que desea eliminar: "));
+            if (borrar < contador) {
+                empleados[borrar][0] = "";
+                empleados[borrar][1] = "";
+                empleados[borrar][2] = "";
+                JOptionPane.showMessageDialog(null, "¡Empleado eliminado exitosamente!");
+            } else if ("".equals(empleados[borrar][0]) && "".equals(empleados[borrar][1]) && "".equals(empleados[borrar][2])) {
+            //deberia de salir este mensaje pero no logre hacerlo funcionar, no sale el mensjae
+                JOptionPane.showMessageDialog(null, "¡El empleado que desea eliminar ya no existe!");
+                return true;
+            } else if (empleados[borrar][0] == null) {
+                JOptionPane.showMessageDialog(null, "¡El empleado que quiere eliminar no existe!");
+                return true;
+            }
+
+            String salida = JOptionPane.showInputDialog("Desea continuar eliminando?(si/no)");
+            if (salida.equalsIgnoreCase("no")) {
+                return true;
+            }
+        }
+
+    }
+
+    public boolean mostrarEmpleados() {
+        if (contador == 0) {
+            JOptionPane.showMessageDialog(null, "¡Aun no hay empleados registrados!");
+        } else {
+            String tabla = "";
+            for (int j = 0; j < contador; j++) {//for para leer en tabla
+                tabla += "(" + j + ")  " + empleados[j][0] + "   " + empleados[j][1] + "   " + empleados[j][2] + "\n";
+
+            }
+            JOptionPane.showMessageDialog(null, "Empleados actuales: \n" + tabla);
+
+        }
+        return true;
+    }
+
+    public boolean modificar() {
+        
+        while (true) {
+            String cedula = JOptionPane.showInputDialog("Ingrese el ID del empleado que desea modificar:");
+            boolean encontrado = false;
+
+        for (int i = 0; i < contador; i++) {
+            if (empleados[i][1].equals(cedula)) {
+                String nuevoNombre = JOptionPane.showInputDialog("Ingrese el nuevo nombre del empleado:");
+                String nuevoRol = JOptionPane.showInputDialog("Ingrese el nuevo rol del empleado:");
+
+                if (nuevoNombre.isEmpty() || nuevoRol.isEmpty()) {
+                    JOptionPane.showMessageDialog(null, "Datos inválidos. Intente de nuevo.");
+                    return false;
+                }
+
+                empleados[i][0] = nuevoNombre;
+                empleados[i][2] = nuevoRol;
+
+                JOptionPane.showMessageDialog(null, "¡Empleado modificado exitosamente!");
+                encontrado = true;
+                break;
+            }
+        }
+
+        if (!encontrado) {
+            JOptionPane.showMessageDialog(null, "¡El empleado no ha sido encontrado!");
+        }
+
+        String salida = JOptionPane.showInputDialog("¿Desea modificar otro empleado? (si/no)");
+        if (salida.equalsIgnoreCase("no")) {
+            return true;
+        }
+    }
+   }
+
+    public boolean buscarEmpleado() {
+        while (true) {
+            //se piden los ID en bucle con su respectiva salida posterior de dar una respuesta
+            String cedula = JOptionPane.showInputDialog("Ingrese el ID del empleado: ");
+
+            // declarar una variable booleana falsa que sirva como validacion
+            boolean encontrado = false;
+
+            //for que lea los datos en el rando del contador que se lleva
+            for (int i = 0; i < contador; i++) {
+                if (empleados[i][1].equals(cedula)) {//si la posicion del ID concuerda con la cedula ingresada a buscar entonces mostrar todos los datos del empleado
+                    JOptionPane.showMessageDialog(null, """
+                                                        Empleado: 
+                                                        Nombre: """ + empleados[i][0]
+                                                        + "\nID: " + empleados[i][1]
+                                                        + "\nRol: " + empleados[i][2]);
+                    encontrado = true; //luego pone el encontrado a true para saber que fue encontrado
+                    break;
+                }
+            }
+            if (!encontrado) {//si el if en este caso se encuentra en false, significa que no fue encontrado, entonces:
+                JOptionPane.showMessageDialog(null, "¡El empleado no ha sido encontrado!");
+            }
+
+            //salida
+            String salida = JOptionPane.showInputDialog("Desea buscar otro empleado?(si/no)");
+            if (salida.equalsIgnoreCase("no")) {
+                return true;
+            }
+        }
+
+    }
+
+    public boolean consultarReserva() {
+        JOptionPane.showMessageDialog(null, "funciona");//prueba
+        return true;//cambiar luego por un booleano que permita generar la accion
+    }
+
+}
